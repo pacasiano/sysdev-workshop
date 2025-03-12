e4a5a4d54e06f279a685ab318677287c0d8738a3
Peter Andre  Casiano <pacasiano@addunet.ph>
2025-03-12 17:38:52 +0800

    second

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image';
-import image from "@/public/nintendo.webp";
+import image from "@/public/nintendo.png";
+import background from "@/public/shadow.png";
 
 export default function Home() {
   return (
     <div className='container'>
-      <Image src={image} alt="image" className='image'/>
+      <div className='image'>
+        <Image src={image} alt="image" className="main-image"/>
+        <Image src={background} alt="background" className="background-image"/>
+      </div>
       <div className='container_two'>
         <div className='title'>Nintendo Switch</div>
         <div className='description'>Home gaming system. Take it on the go.</div>
